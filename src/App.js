5c1959467c2e2fd175c2c183f5abdbae03dce2de
AliAbbasAsif <aliabbasasif89@gmail.com>
2022-10-02 17:29:28 +0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Card, CardContent, CardMedia, Chip, Grid, TextField, ToggleButtonGroup, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
+import Data from './carddata';
+
 
 function App() {
+
+  const [Txt, setTxt] = useState("");
+  const searchtxt = (e) => {
+    setTxt(e.taget.value);
+  }
+  console.warn(Txt)
+  let datasearch = Data.filter(item => {
+    return Object.keys(item).some(key =>
+      item[key].toString().toLowerCase().includes(Txt.toString().toLowerCase())
+    )
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <AppBar sx={{ backgroundColor: "white", color: "black", }}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography variant='h5' align='center' sx={{ textDecoration: "underline", fontWeight: "bold", paddingX: "15px", paddingY: "20px" }}>Products</Typography>
+            <Typography variant='h5' align='center' sx={{ fontWeight: "bold", paddingX: "15px", paddingY: "20px" }}>Manufacture</Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "space-evenly", paddingX: "90px" }}>
+            <img width="150px" src='https://queenschamber.glueup.com/resources/public/images/logo/400x200/5c72c188-a33d-4872-8e0d-f7587caf4277.png' />
+            {/* <TextField  /> */}
+            <TextField color='warning' variant='outlined' fullWidth placeholder="Serach" sx={{ marginLeft: "30px" }} value={Txt} onChange={(e) => {
+              setTxt(e.target.value)
+            }} />
+
+          </Box>
+          <Box>
+            {datasearch.map((item,id)=>(
+              <Chip key={id} label={[...new Set(item.category)]} />
+            ))}
+          </Box>
+        </AppBar>
+      </div>
+
+
+      <div>
+        <Box sx={{ flexGrow: 1, padding: "30px",marginTop:"200px"}}>
+          <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 16 }}>
+
+            {datasearch.map((item, index) => (
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={item.pic}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.desc}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.location}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </div>
     </div>
   );
 }
---
+++ src/carddata.js
@@ -0,0 +1,164 @@
+const Data = [
+    {
+        id: 1,
+        pic: "https://images.olx.com.pk/thumbnails/261948246-400x300.webp",
+        title: "Apple Iphone 14pro",
+        desc: "Rs 129,999",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 2,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Bike 125cc",
+        desc: "Rs 200,000",
+        location: "Karachi",
+        category:"Machine"
+      },
+      {
+        id: 3,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Dell workstation",
+        desc: "Rs 120,000",
+        location: "Lahore",
+        category:"Electronics"
+      },
+      {
+        id: 4,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Treadmill Machine",
+        desc: "Rs 135,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 5,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "S 20 lite",
+        desc: "Rs 15,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 6,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+      {
+        id: 7,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Vivo c21e",
+        desc: "Rs 43,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 8,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Nikon D3200 lens",
+        desc: "Rs 18,500",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 9,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Ps5",
+        desc: "Rs 100,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 10,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Cd playes",
+        desc: "Rs 3,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 11,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Hp laptop",
+        desc: "Rs 50,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 12,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "S21 Ultra",
+        desc: "Rs 185,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 13,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Note 10",
+        desc: "Rs 30,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+      {
+        id: 14,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Nikon Camera",
+        desc: "Rs 30,000",
+        location: "Mirpur khas",
+        category:"Electronics"
+      },
+    
+      {
+        id: 15,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "GTa 5 Online",
+        desc: "Rs 2,000",
+        location: "Mirpur khas",
+        category:"Games"
+      },
+      {
+        id: 16,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+      {
+        id: 17,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+      {
+        id: 18,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+      {
+        id: 19,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+      {
+        id: 20,
+        pic: "https://images.olx.com.pk/thumbnails/289416607-400x300.webp",
+        title: "Oval Chopper",
+        desc: "Rs 950",
+        location: "Mirpur khas",
+        category:"Crockry"
+      },
+]
+export default Data
